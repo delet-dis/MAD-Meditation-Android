102aaa23d8adec4be520a76e0b18f00e8da75846
delet-dis <efimov.036@gmail.com>
2021-02-21 00:56:26 +0700

    updated registration activity

--- app/src/main/java/com/delet_dis/madmeditation/OnboardingActivity.kt
+++ app/src/main/java/com/delet_dis/madmeditation/OnboardingActivity.kt
@@ -1,14 +1,35 @@
 package com.delet_dis.madmeditation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.delet_dis.madmeditation.helpers.WindowHelper
+import com.delet_dis.madmeditation.databinding.ActivityOnboardingBinding
 
 class OnboardingActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityOnboardingBinding
+
+  private lateinit var loginButton: Button
+  private lateinit var registerTextView: TextView
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_onboarding)
+
+    binding = ActivityOnboardingBinding.inflate(layoutInflater)
+    val view = binding.root
+
+    setContentView(view)
 
     WindowHelper.setWindowTransparent(this)
+
+    loginButton = binding.loginButton
+    registerTextView = binding.noAccountHintRegistration
+
+    registerTextView.setOnClickListener {
+      val intent = Intent(this, RegistrationActivity::class.java)
+      startActivity(intent)
+    }
   }
 }
