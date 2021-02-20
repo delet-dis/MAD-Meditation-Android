bfdc97dbd1506378346dac68e4a959bedff458b2
delet-dis <efimov.036@gmail.com>
2021-02-20 22:30:26 +0700

    repaired splashscreen

--- app/src/main/java/com/delet_dis/madmeditation/SplashscreenActivity.kt
+++ app/src/main/java/com/delet_dis/madmeditation/SplashscreenActivity.kt
@@ -1,14 +1,35 @@
 package com.delet_dis.madmeditation
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashscreenActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_splashscreen)
 
-    val intent = Intent(this, MainActivity::class.java)
-    startActivity(intent)
+    setWindowTransparency()
+
+    val handler = Handler(Looper.getMainLooper())
+    handler.postDelayed({
+      val intent = Intent(this, MainActivity::class.java)
+      startActivity(intent)
+      finish()
+    }, 1500)
+
+  }
+
+  private fun setWindowTransparency() {
+    val window = window
+
+    window.statusBarColor = Color.TRANSPARENT
+    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
   }
 }
