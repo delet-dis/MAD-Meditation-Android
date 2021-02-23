f79a890d1a78145ea62423459ce54616c8cc062b
delet-dis <efimov.036@gmail.com>
2021-02-23 19:21:31 +0700

    added avatar loading

--- app/src/main/java/com/delet_dis/madmeditation/MainActivity.kt
+++ app/src/main/java/com/delet_dis/madmeditation/MainActivity.kt
@@ -3,7 +3,12 @@ package com.delet_dis.madmeditation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.delet_dis.madmeditation.databinding.ActivityLoginBinding
+import com.delet_dis.madmeditation.databinding.ActivityMainBinding
 import com.delet_dis.madmeditation.helpers.ConstantsHelper
 import com.delet_dis.madmeditation.helpers.SharedPrefsHelper
 import com.delet_dis.madmeditation.http.common.Common
@@ -13,9 +18,19 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityMainBinding
+
+  private lateinit var userAvatar: ImageView
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    val view = binding.root
+
+    setContentView(view)
+
+    userAvatar = binding.userImage
 
     val loginResponse =
       intent.extras?.getParcelable<LoginResponse>(ConstantsHelper.loginResponseParcelableName)
@@ -28,13 +43,10 @@ class MainActivity : AppCompatActivity() {
         .enqueue(object : Callback<LoginResponse> {
 
           override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
-            SharedPrefsHelper.setLoginState(applicationContext, true)
-
-            val processingIntent = Intent(this@LoginActivity, MainActivity::class.java)
-            processingIntent.putExtra(ConstantsHelper.loginResponseParcelableName, response.body())
-            startActivity(processingIntent)
-            finish()
-
+            Glide.with(applicationContext)
+              .load(response.body()?.avatar)
+              .transition(DrawableTransitionOptions.withCrossFade())
+              .into(userAvatar)
           }
 
           override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
