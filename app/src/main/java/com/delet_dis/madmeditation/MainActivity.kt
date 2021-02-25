fe1073acd0a663860e21fee203d5c65b556058c7
delet-dis <efimov.036@gmail.com>
2021-02-26 00:01:38 +0700

    moved to fragments in mainactivity

--- app/src/main/java/com/delet_dis/madmeditation/MainActivity.kt
+++ app/src/main/java/com/delet_dis/madmeditation/MainActivity.kt
@@ -1,21 +1,16 @@
 package com.delet_dis.madmeditation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.commit
 import com.delet_dis.madmeditation.databinding.ActivityMainBinding
-import com.delet_dis.madmeditation.recyclerView.FeelingsAdapter
 import com.delet_dis.madmeditation.helpers.ConstantsHelper
 import com.delet_dis.madmeditation.helpers.SharedPrefsHelper
 import com.delet_dis.madmeditation.http.common.Common
-import com.delet_dis.madmeditation.model.*
-import com.delet_dis.madmeditation.recyclerView.QuotesAdapter
+import com.delet_dis.madmeditation.model.LoginResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -23,12 +18,8 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
   private lateinit var binding: ActivityMainBinding
 
-  private lateinit var userAvatar: ImageView
-  private lateinit var userNickName: TextView
+  private lateinit var fragmentContainerView: FragmentContainerView
 
-  private lateinit var feelingsView: RecyclerView
-
-  private lateinit var quotesView: RecyclerView
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -38,14 +29,7 @@ class MainActivity : AppCompatActivity() {
 
     setContentView(view)
 
-    userAvatar = binding.userImage
-    userNickName = binding.welcomeText
-
-    feelingsView = binding.feelingsRecycler
-    feelingsView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-
-    quotesView = binding.quotesRecycler
-    quotesView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+    fragmentContainerView = binding.screenFragmentContainerView
 
     val loginResponse =
       intent.extras?.getParcelable<LoginResponse>(ConstantsHelper.loginResponseParcelableName)
@@ -63,7 +47,8 @@ class MainActivity : AppCompatActivity() {
             response: Response<LoginResponse>
           ) {
             processingLoginResponse = response.body()
-            displayUserInfo(processingLoginResponse)
+
+            createMainFragment(processingLoginResponse)
           }
 
           override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
@@ -77,57 +62,20 @@ class MainActivity : AppCompatActivity() {
         })
     } else {
       processingLoginResponse = loginResponse
-      displayUserInfo(processingLoginResponse)
+
+      createMainFragment(processingLoginResponse)
     }
-
-    Common.retrofitService.getFeelingsData()
-      .enqueue(object : Callback<FeelingsResponse> {
-        override fun onResponse(
-          call: Call<FeelingsResponse>,
-          response: Response<FeelingsResponse>
-        ) {
-          val processingFeelingsList: List<Feeling> =
-            response.body()!!.data.sortedWith(compareBy { it.position })
-
-          feelingsView.adapter = FeelingsAdapter(processingFeelingsList)
-        }
-
-        override fun onFailure(call: Call<FeelingsResponse>, t: Throwable) {
-          Toast.makeText(
-            applicationContext,
-            getString(R.string.networkErrorMessage),
-            Toast.LENGTH_SHORT
-          ).show()
-        }
-
-      })
-
-    Common.retrofitService.getQuotesData()
-      .enqueue(object : Callback<QuotesResponse> {
-        override fun onResponse(call: Call<QuotesResponse>, response: Response<QuotesResponse>) {
-          val processingQuotesList: List<Quote> = response.body()!!.data
-          quotesView.adapter = QuotesAdapter(processingQuotesList)
-        }
-
-        override fun onFailure(call: Call<QuotesResponse>, t: Throwable) {
-          Toast.makeText(
-            applicationContext,
-            getString(R.string.networkErrorMessage),
-            Toast.LENGTH_SHORT
-          ).show()
-        }
-
-      })
-
   }
 
-  private fun displayUserInfo(processingLoginResponse: LoginResponse?) {
-    Glide.with(applicationContext)
-      .load(processingLoginResponse?.avatar)
-      .transition(DrawableTransitionOptions.withCrossFade())
-      .into(userAvatar)
-
-    userNickName.text =
-      String.format(getString(R.string.welcomeTextText, processingLoginResponse?.nickName))
+  private fun createMainFragment(processingLoginResponse: LoginResponse?) {
+    supportFragmentManager.commit {
+      setReorderingAllowed(true)
+      add(
+        R.id.screenFragmentContainerView,
+        MainScreenFragment::class.java,
+        bundleOf(Pair(ConstantsHelper.loginResponseParcelableName, processingLoginResponse))
+      )
+    }
   }
+
 }
---
+++ app/src/main/java/com/delet_dis/madmeditation/MainScreenFragment.kt
@@ -0,0 +1,127 @@
+package com.delet_dis.madmeditation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.delet_dis.madmeditation.databinding.FragmentMainScreenBinding
+import com.delet_dis.madmeditation.helpers.ConstantsHelper
+import com.delet_dis.madmeditation.http.common.Common
+import com.delet_dis.madmeditation.model.*
+import com.delet_dis.madmeditation.recyclerView.FeelingsAdapter
+import com.delet_dis.madmeditation.recyclerView.QuotesAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+class MainScreenFragment : Fragment() {
+
+  private lateinit var userAvatar: ImageView
+  private lateinit var userNickName: TextView
+
+  private lateinit var feelingsView: RecyclerView
+
+  private lateinit var quotesView: RecyclerView
+
+  private lateinit var binding: FragmentMainScreenBinding
+
+  private lateinit var loginResponse: LoginResponse
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    binding = FragmentMainScreenBinding.inflate(layoutInflater)
+
+    loginResponse = requireArguments()
+      .getParcelable(ConstantsHelper.loginResponseParcelableName)!!
+
+    return binding.root
+
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    userAvatar = binding.userImage
+    userNickName = binding.welcomeText
+
+    feelingsView = binding.feelingsRecycler
+    feelingsView.layoutManager = LinearLayoutManager(
+      activity,
+      LinearLayoutManager.HORIZONTAL,
+      false
+    )
+
+    quotesView = binding.quotesRecycler
+    quotesView.layoutManager = LinearLayoutManager(
+      activity,
+      LinearLayoutManager.VERTICAL,
+      false
+    )
+
+    displayUserInfo(loginResponse)
+
+    Common.retrofitService.getFeelingsData()
+      .enqueue(object : Callback<FeelingsResponse> {
+        override fun onResponse(
+          call: Call<FeelingsResponse>,
+          response: Response<FeelingsResponse>
+        ) {
+          val processingFeelingsList: List<Feeling> =
+            response.body()!!.data.sortedWith(compareBy { it.position })
+
+          feelingsView.adapter = FeelingsAdapter(processingFeelingsList)
+        }
+
+        override fun onFailure(call: Call<FeelingsResponse>, t: Throwable) {
+          Toast.makeText(
+            activity,
+            getString(R.string.networkErrorMessage),
+            Toast.LENGTH_SHORT
+          ).show()
+        }
+
+      })
+
+    Common.retrofitService.getQuotesData()
+      .enqueue(object : Callback<QuotesResponse> {
+        override fun onResponse(call: Call<QuotesResponse>, response: Response<QuotesResponse>) {
+          val processingQuotesList: List<Quote> = response.body()!!.data
+          quotesView.adapter = QuotesAdapter(processingQuotesList)
+        }
+
+        override fun onFailure(call: Call<QuotesResponse>, t: Throwable) {
+          Toast.makeText(
+            activity,
+            getString(R.string.networkErrorMessage),
+            Toast.LENGTH_SHORT
+          ).show()
+        }
+
+      })
+  }
+
+  private fun displayUserInfo(processingLoginResponse: LoginResponse?) {
+    activity?.let {
+      Glide.with(it)
+        .load(processingLoginResponse?.avatar)
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .into(userAvatar)
+    }
+
+    userNickName.text =
+      String.format(getString(R.string.welcomeTextText, processingLoginResponse?.nickName))
+  }
+
+}
